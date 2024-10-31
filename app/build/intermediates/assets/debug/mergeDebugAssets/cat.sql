9935c7c22b57e2d880a1d41c693462619366a60d
Juniors <murrugarra17904@gmail.com>
2024-10-30 22:09:50 -0500

    inicio_avance

---
+++ app/build/intermediates/assets/debug/mergeDebugAssets/cat.sql
@@ -0,0 +1,11 @@
+-- Insertar categorías
+INSERT INTO CATEGORIAS (Nombre) VALUES ('Herramientas Manuales');
+INSERT INTO CATEGORIAS (Nombre) VALUES ('Herramientas Eléctricas');
+INSERT INTO CATEGORIAS (Nombre) VALUES ('Materiales de Construcción');
+INSERT INTO CATEGORIAS (Nombre) VALUES ('Electricidad');
+INSERT INTO CATEGORIAS (Nombre) VALUES ('Metalmecanica');
+INSERT INTO CATEGORIAS (Nombre) VALUES ('Jardinería');
+INSERT INTO CATEGORIAS (Nombre) VALUES ('Pintura');
+INSERT INTO CATEGORIAS (Nombre) VALUES ('Seguridad');
+INSERT INTO CATEGORIAS (Nombre) VALUES ('Accesorios');
+INSERT INTO CATEGORIAS (Nombre) VALUES ('Mantenimiento del Hogar');
---
+++ app/build/intermediates/assets/debug/mergeDebugAssets/produc.sql
@@ -0,0 +1,23 @@
+-- Insertar productos en la categoría "Herramientas Manuales"
+INSERT INTO PRODUCTOS (Nombre, Descripcion, Precio, Stock, CategoriaId) 
+VALUES ('Martillo', 'Martillo de acero con mango de madera', 25.50, 50, 1);
+INSERT INTO PRODUCTOS (Nombre, Descripcion, Precio, Stock, CategoriaId) 
+VALUES ('Alicates', 'Alicates universales de acero inoxidable', 15.00, 30, 1);
+INSERT INTO PRODUCTOS (Nombre, Descripcion, Precio, Stock, CategoriaId) 
+VALUES ('Llave Inglesa', 'Llave ajustable de 10 pulgadas', 20.00, 40, 1);
+
+-- Insertar productos en la categoría "Herramientas Eléctricas"
+INSERT INTO PRODUCTOS (Nombre, Descripcion, Precio, Stock, CategoriaId) 
+VALUES ('Taladro', 'Taladro eléctrico de 500W', 150.00, 20, 2);
+INSERT INTO PRODUCTOS (Nombre, Descripcion, Precio, Stock, CategoriaId) 
+VALUES ('Sierra Circular', 'Sierra circular de alta precisión', 300.00, 15, 2);
+INSERT INTO PRODUCTOS (Nombre, Descripcion, Precio, Stock, CategoriaId) 
+VALUES ('Lijadora', 'Lijadora orbital de 200W', 85.00, 25, 2);
+
+-- Insertar productos en la categoría "Materiales de Construcción"
+INSERT INTO PRODUCTOS (Nombre, Descripcion, Precio, Stock, CategoriaId) 
+VALUES ('Cemento', 'Saco de cemento de 50kg', 30.00, 100, 3);
+INSERT INTO PRODUCTOS (Nombre, Descripcion, Precio, Stock, CategoriaId) 
+VALUES ('Arena', 'Saco de arena de 40kg', 15.00, 80, 3);
+INSERT INTO PRODUCTOS (Nombre, Descripcion, Precio, Stock, CategoriaId) 
+VALUES ('Ladrillos', 'Paquete de 100 ladrillos estándar', 75.00, 50, 3);
